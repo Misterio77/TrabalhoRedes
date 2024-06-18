99c144fdd29b590dcd6ddda59957011fb74dfce0
Gabriel Fontes <hi@m7.rs>
2024-06-18 13:13:32 -0300

    init poc

---
+++ poc/common.py
@@ -0,0 +1,86 @@
+import requests
+import telnetlib
+import time
+
+TELNET_HOSTNAME = "celaeno"
+
+# Projects 
+
+def list_projects(server):
+    return requests.get(f"{server}/projects").json()
+
+def get_project_by_name(server, name):
+    projects = list_projects(server)
+    return next((p for p in projects if p["name"] == name), None)
+
+def create_project(server, name):
+    payload = {"name": name}
+    return requests.post(f"{server}/projects", json=payload).json()
+
+def get_project(server, name):
+    project = get_project_by_name(server, name)
+    if not project:
+        project = create_project(server, name)
+    return project
+
+# Nodes
+
+def list_nodes(server, project):
+    project_id = project["project_id"]
+    return requests.get(f"{server}/projects/{project_id}/nodes").json()
+
+def get_node_by_name(server, project, name):
+    nodes = list_nodes(server, project)
+    return next((n for n in nodes if n["name"] == name), None)
+
+def create_node(server, project, name, payload):
+    payload["name"] = name
+    project_id = project["project_id"]
+    return requests.post(f"{server}/projects/{project_id}/nodes", json=payload).json()
+
+def get_node(server, project, name, payload, destroy=False):
+    node = get_node_by_name(server, project, name)
+    if destroy and node:
+        destroy_node(server, project, node)
+        node = None
+    if not node:
+        node = create_node(server, project, name, payload)
+    return node
+
+def destroy_node(server, project, node):
+    project_id = project["project_id"]
+    node_id = node["node_id"]
+    requests.delete(f"{server}/projects/{project_id}/nodes/{node_id}")
+
+def start_node(server, project, node):
+    project_id = project["project_id"]
+    node_id = node["node_id"]
+    return requests.post(f"{server}/projects/{project_id}/nodes/{node_id}/start").json()
+
+def stop_node(server, project, node):
+    project_id = project["project_id"]
+    node_id = node["node_id"]
+    return requests.post(f"{server}/projects/{project_id}/nodes/{node_id}/stop").json()
+
+def run_node_command(server, project, node, command):
+    with telnetlib.Telnet(TELNET_HOSTNAME, node["console"]) as session:
+        for line in command.splitlines():
+            session.write(line.encode())
+            session.write(b'\r\n')
+
+def link_nodes(server, project, node1, node2):
+    project_id = project["project_id"]
+    return requests.post(f"{server}/projects/{project_id}/links", json={
+        "nodes": [
+            {
+                "adapter_number": 0,
+                "port_number": 0,
+                "node_id": node1["node_id"],
+            },
+            {
+                "adapter_number": 0,
+                "port_number": 0,
+                "node_id": node2["node_id"],
+            }
+        ]
+    }).json()
---
+++ poc/main.py
@@ -0,0 +1,55 @@
+from common import get_node, get_project, start_node, run_node_command, link_nodes
+
+SERVER = f"https://admin:icmc1234@gns3.m7.rs/v2"
+
+project = get_project(SERVER, "pocpoc")
+
+router1 = get_node(SERVER, project, "R1", {
+    "compute_id": "local",
+    "node_type": "dynamips",
+    "x": 0,
+    "properties": {
+        "platform": "c3745",
+        "nvram": 256,
+        "image": "c3745-adventerprisek9-mz.124-25d.image",
+        "ram": 256, 
+        "slot0": "GT96100-FE",
+        "slot1": "NM-1FE-TX",
+        "slot2": "NM-4T",
+        "system_id": "FTX0945W0MY",
+        "startup_config_content": """
+            hostname irineuuuu
+        """,
+    }
+}, destroy=True)
+start_node(SERVER, project, router1)
+
+router2 = get_node(SERVER, project, "R2", {
+    "compute_id": "local",
+    "node_type": "dynamips",
+    "x": 100,
+    "properties": {
+        "platform": "c3745",
+        "nvram": 256,
+        "image": "c3745-adventerprisek9-mz.124-25d.image",
+        "ram": 256, 
+        "slot0": "GT96100-FE",
+        "slot1": "NM-1FE-TX",
+        "slot2": "NM-4T",
+        "system_id": "FTX0945W0MY",
+        "startup_config_content": """
+            hostname douglassss
+        """,
+    }
+}, destroy=True)
+start_node(SERVER, project, router2)
+
+# run_node_command(SERVER, project, router2, """
+#     enable
+#     conf t
+#     hostname foobar
+#     exit
+#     exit
+# """)
+
+link_nodes(SERVER, project, router1, router2)
